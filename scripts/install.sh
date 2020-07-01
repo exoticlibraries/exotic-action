c64cacfaf97b897e973cd28f2cc18537aff326c6
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-01 03:44:16 +0100

    test downloading libraries

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,5 @@
 
-echo yea yahoo
+wget -O cester.h https://raw.githubusercontent.com/exoticlibraries/libcester/master/include/exotic/cester.h
+sudo mkdir -p /usr/include/exotic/
+sudo mv cester.h /usr/include/exotic/
+echo Done installing cester
