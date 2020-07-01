3d538517ccaf8ce78ac3cb4df75375dec4df23d4
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-01 01:12:10 +0100

    test the action with fair datas

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('child_process');
 
 try {
     const downloadExoticLibraries = getAndSanitizeInputs('download-exotic-libraries', 'boolean', true);
+    const compilerOptsForTests = getAndSanitizeInputs('compiler-options-for-tests', 'array', [ '-pedantic' ]);
+    const runCesterRegression = getAndSanitizeInputs('run-cester-regression', 'boolean', true);
+    const cesterOpts = getAndSanitizeInputs('cester-options', 'array', [ '--cester-noisolation', '--cester-nomemtest' ]);
+    const testFolders = getAndSanitizeInputs('test-folders', 'array', [ 'test/', 'tests/' ]);
     
-    console.log(`Download Exotic Libraries ${downloadExoticLibraries}!`);
+    console.log(`Download Exotic Libraries ${downloadExoticLibraries}`);
+    console.log(`Compiler Options for Tests ${compilerOptsForTests}`);
+    console.log(`Run Cester Regression Tests ${runCesterRegression}`);
+    console.log(`Cester Options ${cesterOpts}`);
+    console.log(`Test Folders ${testFolders}`);
+    
+    console.log()
+    console.log("Test System")
+    exec('ls', (err, stdout, stderr) => {
+      if (err) {
+        return;
+      }
+
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+    console.log()
+    
+    // after
     core.setOutput("tests-passed", true);
     // Get the JSON webhook payload for the event that triggered the workflow
     //const payload = JSON.stringify(github.context.payload, undefined, 2)
