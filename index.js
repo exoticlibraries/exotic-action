4b3312660f7523dd64addb0e430870ba879b8e60
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-02 06:13:34 +0100

    get the list of test folder and run regression

--- index.js
+++ index.js
@@ -73,9 +73,10 @@ function afterDownloadDeps() {
                 var fullPath = path.join(folder, file);
                 var compiler = selectCompilerExec(selectedCompiler, file);
                 console.log("Running test: " + fullPath);
-                var command = `${compiler} ${selectedArch} ${compilerOptsForTests} ${fullPath} -o ${outputName} && ./${outputName} ${cesterOpts}`;
+                var command = `${compiler} ${selectedArch} ${compilerOptsForTests} ${fullPath} -o ${outputName}`;
                 try {
                     await exec.exec(command);
+                    await exec.exec("./${outputName} ${cesterOpts}");
                     params.numberOfTestsRan++;
                 } catch (error) {
                     console.error(error);
