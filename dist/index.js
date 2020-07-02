0e6796e79c6740d932c372fe93195c13e56d9be3
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-02 01:47:58 +0100

    still testing download libraries

--- dist/index.js
+++ dist/index.js
@@ -7257,44 +7257,47 @@ const exec = __webpack_require__(842);
 const http = __webpack_require__(605);
 const fs = __webpack_require__(747);
 
-try {
-    const downloadExLibs = getAndSanitizeInputs('download-exotic-libraries', 'boolean', true);
-    const compilerOptsForTests = getAndSanitizeInputs('compiler-options-for-tests', 'array', [ '-pedantic' ]);
-    const runCesterRegression = getAndSanitizeInputs('run-cester-regression', 'boolean', true);
-    const cesterOpts = getAndSanitizeInputs('cester-options', 'array', [ '--cester-noisolation', '--cester-nomemtest' ]);
-    const testFolders = getAndSanitizeInputs('test-folders', 'array', [ 'test/', 'tests/' ]);
-    
-    if (downloadExLibs === true) {
-        if (downloadExoticLibraries() === false) {
-            throw new Error("Failed to download exotic libraries");
-        }
-    }
-    console.log(`Compiler Options for Tests ${compilerOptsForTests}`);
-    console.log(`Run Cester Regression Tests ${runCesterRegression}`);
-    console.log(`Cester Options ${cesterOpts}`);
-    console.log(`Test Folders ${testFolders}`);
-    
-    /*console.log()
-    console.log("Test System")
-    exec('mkdir -p /usr/include/exotic/; ls /usr/include/exotic/', (err, stdout, stderr) => {
-      if (err) {
-        return;
-      }
+(async function() {
+    try {
+        const downloadExLibs = getAndSanitizeInputs('download-exotic-libraries', 'boolean', true);
+        const compilerOptsForTests = getAndSanitizeInputs('compiler-options-for-tests', 'array', [ '-pedantic' ]);
+        const runCesterRegression = getAndSanitizeInputs('run-cester-regression', 'boolean', true);
+        const cesterOpts = getAndSanitizeInputs('cester-options', 'array', [ '--cester-noisolation', '--cester-nomemtest' ]);
+        const testFolders = getAndSanitizeInputs('test-folders', 'array', [ 'test/', 'tests/' ]);
 
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
-    console.log()*/
-    
-    
-    // after
-    core.setOutput("tests-passed", true);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+        if (downloadExLibs === true) {
+            if (await downloadExoticLibraries() === false) {
+                throw new Error("Failed to download exotic libraries");
+            }
+        }
+        //console.log(`Compiler Options for Tests ${compilerOptsForTests}`);
+        //console.log(`Run Cester Regression Tests ${runCesterRegression}`);
+        //console.log(`Cester Options ${cesterOpts}`);
+        console.log(`Test Folders ${testFolders}`);
+
+        /*console.log()
+        console.log("Test System")
+        exec('mkdir -p /usr/include/exotic/; ls /usr/include/exotic/', (err, stdout, stderr) => {
+          if (err) {
+            return;
+          }
+
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        });
+        console.log()*/
+
+
+        // after
+        core.setOutput("tests-passed", true);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        //const payload = JSON.stringify(github.context.payload, undefined, 2)
+        //console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})()
+
 
 function getAndSanitizeInputs(key, type, defaultValue) {
     var value = core.getInput(key);
--- index.js
+++ index.js
@@ -5,44 +5,47 @@ const exec = require("@actions/exec");
 const http = require('http');
 const fs = require('fs');
 
-try {
-    const downloadExLibs = getAndSanitizeInputs('download-exotic-libraries', 'boolean', true);
-    const compilerOptsForTests = getAndSanitizeInputs('compiler-options-for-tests', 'array', [ '-pedantic' ]);
-    const runCesterRegression = getAndSanitizeInputs('run-cester-regression', 'boolean', true);
-    const cesterOpts = getAndSanitizeInputs('cester-options', 'array', [ '--cester-noisolation', '--cester-nomemtest' ]);
-    const testFolders = getAndSanitizeInputs('test-folders', 'array', [ 'test/', 'tests/' ]);
-    
-    if (downloadExLibs === true) {
-        if (await downloadExoticLibraries() === false) {
-            throw new Error("Failed to download exotic libraries");
-        }
-    }
-    //console.log(`Compiler Options for Tests ${compilerOptsForTests}`);
-    //console.log(`Run Cester Regression Tests ${runCesterRegression}`);
-    //console.log(`Cester Options ${cesterOpts}`);
-    console.log(`Test Folders ${testFolders}`);
-    
-    /*console.log()
-    console.log("Test System")
-    exec('mkdir -p /usr/include/exotic/; ls /usr/include/exotic/', (err, stdout, stderr) => {
-      if (err) {
-        return;
-      }
+(async function() {
+    try {
+        const downloadExLibs = getAndSanitizeInputs('download-exotic-libraries', 'boolean', true);
+        const compilerOptsForTests = getAndSanitizeInputs('compiler-options-for-tests', 'array', [ '-pedantic' ]);
+        const runCesterRegression = getAndSanitizeInputs('run-cester-regression', 'boolean', true);
+        const cesterOpts = getAndSanitizeInputs('cester-options', 'array', [ '--cester-noisolation', '--cester-nomemtest' ]);
+        const testFolders = getAndSanitizeInputs('test-folders', 'array', [ 'test/', 'tests/' ]);
 
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
-    console.log()*/
-    
-    
-    // after
-    core.setOutput("tests-passed", true);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+        if (downloadExLibs === true) {
+            if (await downloadExoticLibraries() === false) {
+                throw new Error("Failed to download exotic libraries");
+            }
+        }
+        //console.log(`Compiler Options for Tests ${compilerOptsForTests}`);
+        //console.log(`Run Cester Regression Tests ${runCesterRegression}`);
+        //console.log(`Cester Options ${cesterOpts}`);
+        console.log(`Test Folders ${testFolders}`);
+
+        /*console.log()
+        console.log("Test System")
+        exec('mkdir -p /usr/include/exotic/; ls /usr/include/exotic/', (err, stdout, stderr) => {
+          if (err) {
+            return;
+          }
+
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        });
+        console.log()*/
+
+
+        // after
+        core.setOutput("tests-passed", true);
+        // Get the JSON webhook payload for the event that triggered the workflow
+        //const payload = JSON.stringify(github.context.payload, undefined, 2)
+        //console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})()
+
 
 function getAndSanitizeInputs(key, type, defaultValue) {
     var value = core.getInput(key);
