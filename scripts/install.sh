0de964eaf519287f82f971173f547c00adea5293
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-02 04:42:11 +0100

    get the list of test folder and run regression

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 HEADER_PATH=/usr/include
-echo "First arg: $1"
-if [ "$sdfff then
+if [ "$1" = "darwin" ]; then
     HEADER_PATH=/usr/local/include
 fi
 wget -O cester.h https://raw.githubusercontent.com/exoticlibraries/libcester/master/include/exotic/cester.h
 sudo mkdir -p $HEADER_PATH/exotic/
-sudo mv cester.h $HEADERadsd
+sudo mv cester.h $HEADER_PATH/exotic/
+echo Done installing cester
