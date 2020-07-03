15f910971f02361859a7a99f112c6106377bb34c
Adewale Azeez <azeezadewale98@gmail.com>
2020-07-04 00:42:07 +0100

    test downloading clang for the windows platform

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -7,9 +7,7 @@ If ($args[2] -eq "clang") {
     echo "installing clang according to the walkthrough on https://ariya.io/2020/01/clang-on-windows"
     choco install --no-progress --yes msys2
     cd C:\tools\msys64\usr\bin
-    [System.Environment]::SetEnvironmentVariable('Path', 
-                        [environment]::GetEnvironmentVariable("Path","User") + ";C:\tools\msys64\usr\bin;C:\tools\msys64\mingw64\bin", 
-                        [System.EnvironmentVariableTarget]::User);
+    $env:Path += ";C:\tools\msys64\usr\bin;C:\tools\msys64\mingw64\bin"
     pacman -Sy
     pacman --noconfirm -S pacman-mirrors
     If ($args[1] -eq "x86") {
@@ -17,4 +15,7 @@ If ($args[2] -eq "clang") {
     } Else {
         pacman --noconfirm -S msys/make mingw64/mingw-w64-x86_64-clang
     }
+    [System.Environment]::SetEnvironmentVariable('Path', 
+                        [environment]::GetEnvironmentVariable("Path","User") + ";C:\tools\msys64\usr\bin;C:\tools\msys64\mingw64\bin", 
+                        [System.EnvironmentVariableTarget]::User);
 }
